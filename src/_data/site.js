8c2a89f2c2b2b669baaf4141a366f587a59a6a6d
p7 <ratulroy20@gmail.com>
2022-07-31 23:05:39 +0600

    name me up

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,7 +2,7 @@ module.exports = {
 	"title": "Eleventy Web Starter",
 	"description": "Minimal boilerplate for new projects built with Eleventy, Tailwind, PostCSS and esbuild",
 	"url": "https://eleventywebstarter.netlify.app",
-	"author": "Chris Collins",
+	"author": "Ratul Roy",
 	"meta_data": {
 		"twitter": "@chrissy_dev",
 		"opengraph_default": "/static/opengraph-default.jpg"
